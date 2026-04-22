03644b4278691964233b24a522aaa62dbdde256d
pjalbrzy <pawel.jalbrzykowski@onem.be>
2026-04-22 21:11:05 +0200

    chore: github api oauth config

--- src/main/java/org/unamur/config/SecurityConfig.java
+++ src/main/java/org/unamur/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.unamur.config;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,16 +20,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable()) // Disable CSRF for APIs
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/error").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/ws-metrics/**").permitAll()
                         .requestMatchers("/metrics/**").authenticated() // Protect your endpoint
-                        .anyRequest().permitAll()
+                        .anyRequest().permitAll() // Maybe change to authenticated()
                 )
-                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
-                .addFilterBefore(new ApiKeyFilter(tokenProperties.getBackendToken()), UsernamePasswordAuthenticationFilter.class);
+                .oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("/FFRONTEND REDIRECTION", true) // TODO
+                )
+
+
+                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin())) // TODO - Remove ?
+                .addFilterBefore(new ApiKeyFilter(tokenProperties.getBackendToken()), UsernamePasswordAuthenticationFilter.class);      // TODO - Remove ?
 
         return http.build();
     }
---
+++ src/main/java/org/unamur/controller/GithubApiController.java
@@ -0,0 +1,9 @@
+package org.unamur.controller;
+
+import org.unamur.api.GitHubApi;
+
+public class GithubApiController implements GitHubApi {
+
+
+
+}
